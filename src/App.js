c2994ea0af2a62a709280571868a225832777e76
OscarB <oballesteros@abstract.cl>
2019-03-28 12:33:50 -0300

    Add form

--- src/App.js
+++ src/App.js
@@ -1,25 +1,49 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import Header from './Components/Header';
+import Formulario from './Components/Formulario';
+import './css/index.css';
 
 class App extends Component {
+
+  token = 'DWKP73ZAPCC7GJRU57Y7';
+
+  state = {
+    categorias: []
+  }
+
+  componentDidMount() {
+    this.obtenerCategorias();
+  }
+
+  obtenerCategorias = async () => {
+  
+    let url = `https://www.eventbriteapi.com/v3/categories/?token=${this.token}&locale=es_ES`;
+
+    await fetch(url)
+    .then(respuesta => {
+      return respuesta.json();
+    })
+    .then(categorias => {
+      this.setState({
+        categorias: categorias.categories
+      });
+    });
+  }
+
+  obtenerEventos = async (busqueda) => {
+    console.log(busqueda);
+  }
+
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        <Header />
+        <div className="uk-container">
+          <Formulario 
+          categorias = { this.state.categorias }
+          obtenerEventos = {this.obtenerEventos } 
+          />
+        </div>
       </div>
     );
   }
